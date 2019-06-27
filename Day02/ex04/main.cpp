38971f11a6834805b3cb7fba171d1bd8de53ab50
oivanyts <oivanyts@student.unit.ua>
2019-06-27 23:38:25 +0300

    Pushed Day02

--- Day02/ex04/main.cpp
+++ Day02/ex04/main.cpp
@@ -9,7 +9,6 @@
 #include <sstream>
 #include "Fixed.hpp"
 
-bool gError;
 std::string cutSpace(char *str) {
 	std::string ret;
 	int len = strlen(str);
@@ -33,18 +32,21 @@ Fixed calculate(std::string expr) {
 		pos = expr.find('+');
 		return (calculate(expr.substr(0, pos)) + calculate(expr.substr(pos + 1)));
 	}
-	else if (expr.find('-', 1) != std::string::npos)
+	else if (expr.find('-', 1) != std::string::npos && (pos =  expr.find('-', 1))
+	&& expr[pos - 1] != '/' && expr[pos - 1] != '*')
 	{
-		pos = expr.find('-');
 		return (calculate(expr.substr(0, pos)) - calculate(expr.substr(pos + 1)));
 	}
 	else if (expr.find('/') != std::string::npos)
 	{
 		pos = expr.find('/');
-		Fixed div = calculate(expr.substr(pos + 1));
-		if (div == 0)
-			return (0);
-		return (calculate(expr.substr(0, pos)) / div);
+		Fixed div1 = calculate(expr.substr(0, pos));
+		Fixed div2 = calculate(expr.substr(pos + 1));
+		if (div2 == 0)
+		{
+			exit (EXIT_FAILURE);
+		}
+		return ( div1/ div2 );
 	}
 	else if (expr.find('*') != std::string::npos)
 	{
@@ -80,7 +82,7 @@ Fixed cutBrackets(std::string expr) {
 				std::stringstream	ret;
 				ret << res.toFloat();
 				expr.replace(opBrStart, i - opBrStart + 1, ret.str());
-				return (cutBrackets(expr));
+				return (res = cutBrackets(expr));
 			}
 		}
 	}
@@ -90,7 +92,7 @@ Fixed cutBrackets(std::string expr) {
 bool valideExpr(std::string expr)
 {
 	int n = 0;
-	const std::string tockens("+-().*/");
+	const std::string tockens("().+-*/");
 
 	for (uint i = 0; i < expr.length(); ++i)
 	{
@@ -101,15 +103,15 @@ bool valideExpr(std::string expr)
 			std::cout << "BAD TOCKEN ["<<  expr[i] << "]" << std::endl;
 			return false;
 		}
-		else if (expr[i + 1] && tockens.find(expr[i], 5) != std::string::npos &&
+		else if (expr[i + 1] && tockens.find(expr[i], 3) != std::string::npos &&
 				tockens.find(expr[i + 1], 5) != std::string::npos)
 		{
-			std::cout << "BAD TOCKEN ["<< expr[i] << expr[i + 1] << "]" << i << std::endl;
+			std::cout << "BAD TOCKEN ["<< expr[i] << expr[i + 1] << "]" << std::endl;
 			return false;
 		}
 		if (expr[i] == '(')
 			n++;
-		if (expr[i] == ')')
+		if (expr[i] == ')' && n >= 0)
 			n--;
 	}
 	if (n != 0)
@@ -128,7 +130,9 @@ int	main(int ac , char *av[] )
 		std::string str(av[1]);
 		str = cutSpace(av[1]);
 		if (valideExpr(str))
+		{
 			std::cout << cutBrackets(str) << std::endl;
+		}
 	}
 	return 0;
 }
