0aa4056f1eeedf1e94e54729bd229650f181f501
oivanyts <oivanyts@student.unit.ua>
2019-06-27 22:43:53 +0300

    day02 ex04 eval_exp (no validation)

--- Day02/ex04/main.cpp
+++ Day02/ex04/main.cpp
@@ -9,38 +9,7 @@
 #include <sstream>
 #include "Fixed.hpp"
 
-Fixed	do_op(Fixed &a, Fixed &b,char op)
-{
-	if (op == '+')
-		return(a + b);
-	else if (op == '-')
-		return(a - b);
-	else if (op == '*')
-		return(a * b);
-	else if (op == '/')
-		return(a / b);
-	return (0);
-}
-
-//Fixed calc(std::stringstream count)
-//{
-//	std::cout << " GET " << count << std::endl;
-//	return Fixed(0);
-////}
-//char peek()
-//{
-//
-//}
-//
-//char get()
-//{
-//
-//}
-
-//Fixed expresion(std::string basicString) {
-//	return Fixed();
-//}
-
+bool gError;
 std::string cutSpace(char *str) {
 	std::string ret;
 	int len = strlen(str);
@@ -72,7 +41,10 @@ Fixed calculate(std::string expr) {
 	else if (expr.find('/') != std::string::npos)
 	{
 		pos = expr.find('/');
-		return (calculate(expr.substr(0, pos)) / calculate(expr.substr(pos + 1)));
+		Fixed div = calculate(expr.substr(pos + 1));
+		if (div == 0)
+			return (0);
+		return (calculate(expr.substr(0, pos)) / div);
 	}
 	else if (expr.find('*') != std::string::npos)
 	{
@@ -82,14 +54,13 @@ Fixed calculate(std::string expr) {
 	std::stringstream	ret(expr);
 	float done;
 	ret >> done;
-	std::cout << "result " << done << std::endl;
 	return Fixed(done);
 }
 
-Fixed cut_brackets(std::string expr) {
+Fixed cutBrackets(std::string expr) {
 
 	Fixed res;
-	int op_br_st = expr.find('(');
+	int opBrStart = expr.find('(');
 	int opened = 0;
 
 	if (expr.find('(') == std::string::npos)
@@ -105,24 +76,46 @@ Fixed cut_brackets(std::string expr) {
 			opened--;
 			if (!opened)
 			{
-				res = cut_brackets(expr.substr(op_br_st + 1, i - op_br_st - 1));
+				res = cutBrackets(expr.substr(opBrStart + 1, i - opBrStart - 1));
 				std::stringstream	ret;
 				ret << res.toFloat();
-				expr.replace(op_br_st, i - op_br_st + 1, ret.str());
-				return (cut_brackets(expr));
+				expr.replace(opBrStart, i - opBrStart + 1, ret.str());
+				return (cutBrackets(expr));
 			}
 		}
 	}
 	return (1);
 }
 
-//Fixed evaluete(std::string expr)
-//{
-//	Fixed	res;
-//
-//	res = cut_brackets(expr);
-//	return res;
-//}
+bool valideExpr(std::string expr)
+{
+	int n = 0;
+	const std::string tockens("+-().*/");
+
+	for (uint i = 0; i < expr.length(); ++i)
+	{
+		if (isdigit(expr[i]))
+			continue;
+		else if (tockens.find(expr[i], 0) == std::string::npos)
+		{
+			std::cout << "BAD TOCKEN ["<<  expr[i] << "]" << std::endl;
+			return false;
+		}
+		else if (expr[i + 1] && tockens.find(expr[i], 5) != std::string::npos &&
+				tockens.find(expr[i + 1], 5) != std::string::npos)
+		{
+			std::cout << "BAD TOCKEN ["<< expr[i] << expr[i + 1] << "]" << i << std::endl;
+			return false;
+		}
+		if (expr[i] == '(')
+			n++;
+		if (expr[i] == ')')
+			n--;
+	}
+	if (n != 0)
+		std::cout << "BAD BRACKETS"  << std::endl;
+	return (n == 0);
+}
 
 int	main(int ac , char *av[] )
 {
@@ -132,12 +125,10 @@ int	main(int ac , char *av[] )
 	}
 	else
 	{
-		std::string str;
-
+		std::string str(av[1]);
 		str = cutSpace(av[1]);
-		std::cout<<str<<std::endl;
-		Fixed result = cut_brackets(str);
-		std::cout << result;
+		if (valideExpr(str))
+			std::cout << cutBrackets(str) << std::endl;
 	}
 	return 0;
 }
